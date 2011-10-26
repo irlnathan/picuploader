684607f22152616406fd60976a19f734a05ba2e7
Ponzicoder <ponzicoder@yahoo.com>
2011-10-26 16:38:04 -0500

    another

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,11 +1,13 @@
 class Pic < ActiveRecord::Base
   
-  has_attached_file :pic, :styles => { :thumb => "200x200>"},
+  has_attached_file :pic, #:styles => { :thumb => "200x200>"},
  
   :storage => :s3,
   :bucket => '5my-pics',
   :s3_credentials => {
-    :access_key_id => ENV['AKIAIBXZXDJI7GO3Q6VA'],
-    :secret_access_key => ENV['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']
+     :access_key_id => 'AKIAIBXZXDJI7GO3Q6VA',
+      :secret_access_key => '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'
+   # :access_key_id => ENV['AKIAIBXZXDJI7GO3Q6VA'],
+   # :secret_access_key => ENV['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']
   }
 end
