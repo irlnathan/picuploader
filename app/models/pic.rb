84e6193b999d0feba83ee67e39126fc2220d7754
Ponzicoder <ponzicoder@yahoo.com>
2011-10-26 14:39:21 -0500

    changed bucket name

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -2,7 +2,7 @@ class Pic < ActiveRecord::Base
   
   has_attached_file :pic, :styles => { :thumb => "200x200>"}
   :storage => :s3,
-  :bucket => "1wlpics",
+  :bucket => "5my-pics",
   :s3_credentials => {
     :access_key_id => ENV['AKIAIBXZXDJI7GO3Q6VA'],
     :secret_access_key => ENV['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']
