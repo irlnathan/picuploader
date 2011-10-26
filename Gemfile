8ab41813d89f66ba02133d06d57f3c7a9aa6990a
Ponzicoder <ponzicoder@yahoo.com>
2011-10-26 15:40:17 -0500

    added aws-s3 gem

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.0.9'
 
 gem 'sqlite3'
 gem "paperclip", "~> 2.4"
+gem 'aws-s3'
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,8 +1,9 @@
 class Pic < ActiveRecord::Base
   
   has_attached_file :pic, :styles => { :thumb => "200x200>"}
+ 
   :storage => :s3,
-  :bucket => "5my-pics",
+  :bucket => '5my-pics',
   :s3_credentials => {
     :access_key_id => ENV['AKIAIBXZXDJI7GO3Q6VA'],
     :secret_access_key => ENV['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']
