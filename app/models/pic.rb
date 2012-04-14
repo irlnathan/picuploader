7bbab9cd6692c269bc21ef4e6bce0e26a5f412ec
Ponzicoder <ponzicoder@yahoo.com>
2012-04-14 14:02:22 -0500

    noid

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -5,9 +5,7 @@ class Pic < ActiveRecord::Base
   :storage => :s3,
   :bucket => '5my-pics',
   :s3_credentials => {
-     :access_key_id => 'AKIAIBXZXDJI7GO3Q6VA',
-      :secret_access_key => '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'
-   # :access_key_id => ENV['AKIAIBXZXDJI7GO3Q6VA'],
-   # :secret_access_key => ENV['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']
+     :access_key_id => 'ID',
+      :secret_access_key => 'ID'
   }
 end
